a8aab90f2538ea3efceb0764664468c909929510
CezaryKlarecki <cezary.klarecki@gmail.com>
2021-04-28 18:18:29 +0200

    Dodanie kontruktorów do Zadania 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,17 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-    //p1
-    public double x1;
-    public double y1;
 
-   //p2
-    public double x2;
-    public double y2;
+  public double x1;
+  public double y1;
 
-
-
+  public Point(double x1, double y1) {
+    this.x1 = x1;
+    this.y1 = y1;
   }
+
+
+ // public double x2;
+ // public double y2;
+
+ // public Point(double x2, double y2) {
+  //  this.x2 = x2;
+  //  this.y2 = y2;
+  }
+
+
 
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointMainClass.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointMainClass.java
@@ -4,18 +4,15 @@ public class PointMainClass {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point();
-    p1.x1 = 3;
-    p1.y1 = 4;
-    Point p2 = new Point();
-    p2.x2 = 7;
-    p2.y2 = 11;
-    System.out.println("Odległość między punktami (" + p1.x1 + "," + p1.y1 + ") i (" + p2.x2 + "," + p2.y2 + ") = " + distance(p1,p2));
+    Point p1 = new Point(3, 4);
+    Point p2 = new Point(7, 11);
+
+    System.out.println("Odległość między punktami (" + p1.x1 + "," + p1.y1 + ") i (" + p2.x1+ "," + p2.y1+ ") = " + distance(p1,p2));
 }
 
 
   public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.x2 - p1.x1),2) + Math.pow((p2.y2 - p1.y1),2));
+    return Math.sqrt(Math.pow((p2.x1- p1.x1),2) + Math.pow((p2.y1 - p1.y1),2));
 
 
   }
