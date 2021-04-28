e6ecc27d1e2569f8c72cc182b17af495f3913a0c
CezaryKlarecki <cezary.klarecki@gmail.com>
2021-04-28 20:25:44 +0200

    Poprawki zadania domowego nr 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointMainClass.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointMainClass.java
@@ -4,10 +4,10 @@ public class PointMainClass {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(3, 4, 7, 11);
-    //Point p2 = new Point(7, 11);
+    Point p1 = new Point(3, 4);
+    Point p2 = new Point(7, 11);
 
-    System.out.println("Odległość między punktami (" + p1.x1 + "," + p1.y1 + ") i (" + p1.x2+ "," + p1.y2+ ") = " + p1.distance());
+    System.out.println("Odległość między punktami (" + p1.x1 + "," + p1.y1 + ") i (" + p2.x1+ "," + p2.y1+ ") = " + p1.distance(p2));
 }
 
 
