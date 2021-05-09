1f210d6cecdc839c8c325c56a3c36811969c2852
CezaryKlarecki <cezary.klarecki@gmail.com>
2021-05-09 22:14:57 +0200

    BrowserType

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
   private WebDriver wd;
 
   @BeforeMethod(alwaysRun = true)
