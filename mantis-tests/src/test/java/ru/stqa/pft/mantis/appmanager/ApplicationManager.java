f4d887a0f78ccb008c6ffa773e0ffaeace475462
CezaryKlarecki <cezary.klarecki@gmail.com>
2021-06-22 19:05:21 +0200

    8.3 HttpSession

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -51,4 +51,13 @@ public class ApplicationManager {
     logout();
     wd.quit();
   }
+
+  public HttpSession newSession(){
+return new HttpSession (this);
+
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
