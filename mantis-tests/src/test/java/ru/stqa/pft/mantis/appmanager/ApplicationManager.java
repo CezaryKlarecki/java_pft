096a8dfe98550b43a89682331eb80287e9bca735
CezaryKlarecki <cezary.klarecki@gmail.com>
2021-06-22 20:07:25 +0200

    8.4 leniwa inicjalizacja pomocnikow

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.junit.JUnit4TestRunner;
 
 import java.io.File;
 import java.io.FileReader;
@@ -18,6 +19,7 @@ public class ApplicationManager {
   private final Properties properties;
   private WebDriver wd;
   private String browser;
+  private RegistrationHelper registrationHelper;
 
 
   public ApplicationManager(String browser) {
@@ -29,35 +31,39 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
 
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-
-    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-    wd.get(properties.getProperty("web.baseUrl"));
-
-  }
-
-
-  public void logout() {
-    wd.findElement(By.linkText("Logout")).click();
   }
 
   public void stop() {
-    logout();
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
-
   public HttpSession newSession(){
 return new HttpSession (this);
-
   }
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+  public WebDriver getDriver() {
+    if(wd == null){
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
