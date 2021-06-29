f8af37dbf12926072bc753b6b3bb2debce7e13dc
CezaryKlarecki <cezary.klarecki@gmail.com>
2021-06-29 22:49:05 +0200

    RestAssuredTests

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -4,18 +4,21 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.message.BasicNameValuePair;
+import com.jayway.restassured.RestAssured;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.Set;
 
 
+public class RestAssuredTests {
+  @BeforeClass
+  public void init() {
+    RestAssured.authentication = RestAssured.basic("28accbe43ea112d9feb328d2c00b3eed", "");
+  }
 
-public class RestTests {
   @Test
   public void testCreateIssue() throws IOException {
     Set<Issue> oldIssues = getIssues();
@@ -27,27 +30,22 @@ public class RestTests {
   }
 
 
-
   private Set<Issue> getIssues() throws IOException {
-    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
-            .returnContent().asString();
+    String json = RestAssured.get("http://demo.bugify.com/api/issues.json").asString();
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
 
-    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
+    }.getType());
   }
-
 
 
   private int createIssue(Issue newIssue) throws IOException {
-    String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
-            .bodyForm(new BasicNameValuePair("subejct", new Issue().getSubject())
-                    , new BasicNameValuePair("description", new Issue().getDescription())))
-            .returnContent().asString();
+    String json = RestAssured.given()
+            .parameter("subejct", new Issue().getSubject())
+            .parameter("description", new Issue().getDescription())
+            .post("http://demo.bugify.com/api/issues.json").asString();
     JsonElement parsed = new JsonParser().parse(json);
     return parsed.getAsJsonObject().get("issue_id").getAsInt();
-  }
-  private Executor getExecutor() {
-    return Executor.newInstance().auth("28accbe43ea112d9feb328d2c00b3eed", "");
   }
 }
